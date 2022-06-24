a4d50071d8559abe31f2e18f6868aa2c4d9b8fb0
Koki Yasueda <kouzero.28925224@gmail.com>
2022-06-24 21:42:03 +0900

    commit

--- quiz.sql
+++ quiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2022-03-24 23:22:12
+-- 生成日時: 2022-06-24 21:39:32
 -- サーバのバージョン： 10.4.22-MariaDB
 -- PHP のバージョン: 7.4.27
 
@@ -57,7 +57,8 @@ CREATE TABLE `likes` (
 INSERT INTO `likes` (`id`, `user_id`, `post_id`, `created_at`, `updated_at`) VALUES
 (1, 9, 4, '2022-03-24 04:58:54', '2022-03-24 04:58:54'),
 (3, 9, 6, '2022-03-24 05:06:04', '2022-03-24 05:06:04'),
-(8, 1, 7, '2022-03-24 11:59:20', '2022-03-24 11:59:20');
+(8, 1, 7, '2022-03-24 11:59:20', '2022-03-24 11:59:20'),
+(13, 1, 38, '2022-06-24 11:19:11', '2022-06-24 11:19:11');
 
 -- --------------------------------------------------------
 
@@ -117,7 +118,8 @@ CREATE TABLE `posts` (
 INSERT INTO `posts` (`id`, `user_id`, `user_name`, `quiz`, `hint`, `answer`, `created_at`, `updated_at`) VALUES
 (4, 1, 'root', 'お年寄りがバスに乗ってきました。\r\n大きな荷物を重そうに持っています。\r\nしかし、だれも席をゆずりません。なぜでしょう？', 'いろいろな車内の状況を考えてみよう！', '乗客がいなかったから！', '2022-03-22 02:44:22', '2022-03-22 02:44:22'),
 (6, 2, 'test', '人間の心臓は1人に1つあるものです。\r\nしかし2つも3つも持っている人がいます。\r\nどんな人でしょう？', '人の一生を考えてみよう！', '妊婦\r\n本人の心臓とお腹の中の赤ちゃんの心臓。双子の場合、合わせて3つになります。', '2022-03-22 03:09:25', '2022-03-22 03:09:25'),
-(7, 1, 'root', '国際会議の最中に突然停電になりました。\r\n一番初めに「電気を付けろ！」と\r\n叫んだのは、どこの国の人でしょう？', '文から探すのではなくその状況を想像しよう！！', '日本\r\n「電気を付けろ！」と日本語で言っている。', '2022-03-22 23:07:02', '2022-03-24 20:59:32');
+(7, 1, 'root', '国際会議の最中に突然停電になりました。\r\n一番初めに「電気を付けろ！」と\r\n叫んだのは、どこの国の人でしょう？', '文から探すのではなくその状況を想像しよう！！', '日本\r\n「電気を付けろ！」と日本語で言っている。', '2022-03-22 23:07:02', '2022-03-24 20:59:32'),
+(38, 1, 'root', '1+1=?', '計算ではありません！', '田\r\n1+1＝　→　H＝　→　田', '2022-04-07 04:45:40', '2022-04-07 04:45:40');
 
 -- --------------------------------------------------------
 
@@ -140,13 +142,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'ヤスエダコウキ', 'kouzero.28925224@gmail.com', '$2y$10$1M1ElZqBJKUUHGkOfVq8Z.2A4GSouS8p7e9T6pxDT.THtrp5bm7Ey', 'Jc6Mam8mQYnEljPZtW4qemq4LFqHXXao3gZKIjUJToj5agcwRYg0dQxYL3rd', '2022-03-20 17:58:27', '2022-03-24 05:25:18'),
-(2, 'test', 'kou-sross-28925224@yahoo.ne.jp', '$2y$10$4BfyIf0sBP3FdFeQR1aQFePaLM5HSSw314nP/E.VGkIedOdPePCDW', NULL, '2022-03-21 18:09:00', '2022-03-21 18:09:00'),
-(3, 'コウキ', 'kou@gmail.com', '$2y$10$jtkUVNNvJh.RyCEaKqoYjuw8UFHwi0yT1XCIykSNZMdME9GcqQZVS', NULL, '2022-03-22 14:31:07', '2022-03-22 14:31:07'),
 (5, '管理者', 'quizroot.laravel@gmail.com', '$2y$10$Yu0cOrfaCWqobtImYO6cxeW.5POy5JXYR2C.Lu78r6zTFNa1WqJdK', NULL, '2022-03-23 15:53:53', '2022-03-23 15:53:53'),
-(7, 'コウキ', 'koukoukou@gmail.com', '$2y$10$O0EzQ9fyUdYost0IF/A1oeUIqKfC6RzEp/qwcJMILEDDQ8oSiawve', NULL, '2022-03-24 03:14:11', '2022-03-24 03:14:11'),
-(8, 'ヤスエダコウキ', 'kou5555@gmail.com', '$2y$10$dFjiSOBxghGWZ3m/CWKgd.v/Q33Q1ZztKJLI2gMv0ERh4o709H7E.', NULL, '2022-03-24 03:15:48', '2022-03-24 03:15:48'),
-(9, 'あ', 'kouaaaaaa@gmail.com', '$2y$10$7LHGmkF5EMhwrrozuKJqd.P3z6a222JaGiu6GSLgJWdM8gRM2uQZW', NULL, '2022-03-24 03:16:29', '2022-03-24 03:16:29'),
 (10, 'Test', 'Test@Test', '$2y$10$1ImsPWNqL/8PuvKN.PsFBu0LB9AFjgx.1r/Mn00d/amrbxXkxdG46', NULL, '2022-03-24 14:13:30', '2022-03-24 14:13:30'),
 (11, 'TestUser', 'Test@user', '$2y$10$A8hZzux4J/wxF21iDHNY8uW/xkwIbuH.AyJj2F7nqJeuGwswN5cG.', NULL, '2022-03-24 14:14:26', '2022-03-24 14:14:26');
 
@@ -226,7 +222,7 @@ ALTER TABLE `failed_jobs`
 -- テーブルの AUTO_INCREMENT `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- テーブルの AUTO_INCREMENT `migrations`
@@ -238,7 +234,7 @@ ALTER TABLE `migrations`
 -- テーブルの AUTO_INCREMENT `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- テーブルの AUTO_INCREMENT `users`
