84db0107159677f560de36ac9f6572c4a652bc65
Koki Yasueda <kouzero.28925224@gmail.com>
2022-06-27 00:23:46 +0900

    commit

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title')</title>
-        <link rel="stylesheet" href="{{ mix('css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <script src="{{ mix('js/app.js') }}"></script>
     </head>
 @endsection
